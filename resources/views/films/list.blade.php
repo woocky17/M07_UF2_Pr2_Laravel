a9097dfaf04e1254bafb6e7334e00fc837dbab92
Woocky17 <davidortetsabater@gmail.com>
2025-01-17 18:10:37 +0100

    feature/ Add list country and duration data for each film

--- resources/views/films/list.blade.php
+++ resources/views/films/list.blade.php
@@ -4,24 +4,26 @@
     <FONT COLOR="red">No se ha encontrado ninguna película</FONT>
 @else
     <div align="center">
-    <table border="1">
-        <tr>
-            @foreach($films as $film)
-                @foreach(array_keys($film) as $key)
-                    <th>{{$key}}</th>
-                @endforeach
-                @break
-            @endforeach
-        </tr>
-
-        @foreach($films as $film)
+        <table border="1">
             <tr>
-                <td>{{$film['name']}}</td>
-                <td>{{$film['year']}}</td>
-                <td>{{$film['genre']}}</td>
-                <td><img src={{$film['img_url']}} style="width: 100px; heigth: 120px;" /></td>
+                @foreach($films as $film)
+                    @foreach(array_keys($film) as $key)
+                        <th>{{$key}}</th>
+                    @endforeach
+                    @break
+                @endforeach
             </tr>
-        @endforeach
-    </table>
-</div>
+
+            @foreach($films as $film)
+                <tr>
+                    <td>{{$film['name']}}</td>
+                    <td>{{$film['year']}}</td>
+                    <td>{{$film['genre']}}</td>
+                    <td>{{$film['country']}}</td>
+                    <td>{{$film['duration']}}</td>
+                    <td><img src={{$film['img_url']}} style="width: 100px; heigth: 120px;" /></td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @endif
